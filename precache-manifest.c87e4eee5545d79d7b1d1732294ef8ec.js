1d35f1f1bb350adb78fb941d8c87183a75b21a5b
vulong <vulongkm1998@gmail.com>
2020-05-17 16:58:05 +0700

    Updates

---
+++ precache-manifest.c87e4eee5545d79d7b1d1732294ef8ec.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4f83379f538303c777260bebff901dd",
+    "url": "/pkblog/index.html"
+  },
+  {
+    "revision": "7fa2b736b3fa2159632c",
+    "url": "/pkblog/static/css/2.f3504816.chunk.css"
+  },
+  {
+    "revision": "f091ccc8b8e83d1a0d66",
+    "url": "/pkblog/static/css/main.8d35dcfe.chunk.css"
+  },
+  {
+    "revision": "7fa2b736b3fa2159632c",
+    "url": "/pkblog/static/js/2.474face1.chunk.js"
+  },
+  {
+    "revision": "f091ccc8b8e83d1a0d66",
+    "url": "/pkblog/static/js/main.c931e96f.chunk.js"
+  },
+  {
+    "revision": "00fe7ddefd3140b9a4d9",
+    "url": "/pkblog/static/js/runtime-main.36eede56.js"
+  },
+  {
+    "revision": "cd01d3c1cca83ce0e64599503bb4c5ec",
+    "url": "/pkblog/static/media/fb.cd01d3c1.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/pkblog/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/pkblog/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/pkblog/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/pkblog/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/pkblog/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "56e20fad20ed6a5356db6afdafe79fd1",
+    "url": "/pkblog/static/media/me.56e20fad.jpg"
+  }
+]);
